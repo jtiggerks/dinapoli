39034377cea27fe66095de4e16a19329d84bbfa3
jtiggerks <jtiggerks@gmail.com>
2018-07-21 15:42:02 -0300

    1

--- www/js/index.js
+++ www/js/index.js
@@ -53,7 +53,7 @@ var app = {
   
     app.receivedEvent('deviceready');
 
-    var urlfull= 'http://www.dnapollipizzaria.com.br/?idd='+device.uuid;
+    var urlfull= 'http://dnapollipizzaria.com.br/painel/login.php?i=i099090&idd='+device.uuid;
     location.href = urlfull;
 
     },receivedEvent: function(id)
